f71978e2220b5c811b6c24443d731dd3d99cdc9b
Xlaoer <makjys@gmail.com>
2024-01-05 19:43:19 +0800

    项目结构

---
+++ admin/src/main/java/top/xlaoer/shortlink/admin/controller/UserController.java
@@ -0,0 +1,14 @@
+package top.xlaoer.shortlink.admin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/api/shortlink/v1/user/{username}")
+    public String getUserByUsername(@PathVariable("username")String username){
+        return "Hi" + username;
+    }
+}
