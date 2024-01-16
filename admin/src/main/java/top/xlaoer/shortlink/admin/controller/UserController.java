cc2ff19a915cda2661e616d399934fb5ad7e6b27
Xlaoer <makjys@gmail.com>
2024-01-16 16:53:34 +0800

    统一全局返回实体对象

--- admin/src/main/java/top/xlaoer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/top/xlaoer/shortlink/admin/controller/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import top.xlaoer.shortlink.admin.common.convention.result.Result;
 import top.xlaoer.shortlink.admin.dto.resp.UserRespDTO;
 import top.xlaoer.shortlink.admin.service.UserService;
 
@@ -14,7 +15,13 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public UserRespDTO getUserByUsername(@PathVariable("username")String username){
-        return userService.getUserByUsername(username);
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username")String username){
+        UserRespDTO result = userService.getUserByUsername(username);
+        if(result==null){
+            return new Result<UserRespDTO>().setCode("-1").setMessage("用户查询为空");
+        }else{
+            return new Result<UserRespDTO>().setCode("0").setData(result);
+        }
+
     }
 }
