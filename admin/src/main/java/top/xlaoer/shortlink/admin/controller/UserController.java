9bbb51b5fb4164277db52aa46b8cf18ee16f6975
Xlaoer <makjys@gmail.com>
2024-02-01 16:02:02 +0800

    feature: 用户注册

--- admin/src/main/java/top/xlaoer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/top/xlaoer/shortlink/admin/controller/UserController.java
@@ -2,12 +2,10 @@ package top.xlaoer.shortlink.admin.controller;
 
 import cn.hutool.core.bean.BeanUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import top.xlaoer.shortlink.admin.common.convention.result.Result;
 import top.xlaoer.shortlink.admin.common.convention.result.Results;
+import top.xlaoer.shortlink.admin.dto.req.UserRegisterReqDTO;
 import top.xlaoer.shortlink.admin.dto.resp.UserActualRespDTO;
 import top.xlaoer.shortlink.admin.dto.resp.UserRespDTO;
 import top.xlaoer.shortlink.admin.service.UserService;
@@ -21,7 +19,7 @@ public class UserController {
     /**
      * 根据用户名查询用户信息
      */
-    @GetMapping("/api/shortlink/v1/user/{username}")
+    @GetMapping("/api/short-link/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username")String username){
         return Results.success(userService.getUserByUsername(username));
     }
@@ -29,7 +27,7 @@ public class UserController {
     /**
      * 根据用户名查询无脱敏用户信息
      */
-    @GetMapping("/api/shortlink/v1/actual/user/{username}")
+    @GetMapping("/api/short-link/v1/actual/user/{username}")
     public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username")String username){
         return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
     }
@@ -40,5 +38,14 @@ public class UserController {
     @GetMapping("/api/short-link/v1/user/has-username")
     public Result<Boolean> hasUsername(@RequestParam("username") String username) {
         return Results.success(userService.hasUsername(username));
+    }
+
+    /**
+     * 注册用户
+     */
+    @PostMapping("/api/short-link/v1/user")
+    public Result<Void> register(@RequestBody UserRegisterReqDTO requestParam) {
+        userService.register(requestParam);
+        return Results.success();
     }
 }
--- admin/src/main/java/top/xlaoer/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/top/xlaoer/shortlink/admin/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package top.xlaoer.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -9,8 +10,12 @@ import top.xlaoer.shortlink.admin.common.convention.exception.ClientException;
 import top.xlaoer.shortlink.admin.common.enums.UserErrorCodeEnum;
 import top.xlaoer.shortlink.admin.dao.entity.UserDO;
 import top.xlaoer.shortlink.admin.dao.mapper.UserMapper;
+import top.xlaoer.shortlink.admin.dto.req.UserRegisterReqDTO;
 import top.xlaoer.shortlink.admin.dto.resp.UserRespDTO;
 import top.xlaoer.shortlink.admin.service.UserService;
+
+import static top.xlaoer.shortlink.admin.common.enums.UserErrorCodeEnum.USER_NAME_EXIST;
+import static top.xlaoer.shortlink.admin.common.enums.UserErrorCodeEnum.USER_SAVE_ERROR;
 
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
@@ -34,5 +39,16 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class).eq(UserDO::getUsername, username);
         UserDO userDO = baseMapper.selectOne(queryWrapper);
         return userDO!=null;
+    }
+
+    @Override
+    public void register(UserRegisterReqDTO requestParam) {
+        if (hasUsername(requestParam.getUsername())) {
+            throw new ClientException(USER_NAME_EXIST);
+        }
+        int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+        if (inserted < 1) {
+            throw new ClientException(USER_SAVE_ERROR);
+        }
     }
 }
