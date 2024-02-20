7e0895f6d41b2569e70d0c55bb962168d74f4eb6
Xlaoer <makjys@gmail.com>
2024-02-20 16:29:32 +0800

    fix: 引入mybatisplus分页插件&添加短链接favicon字段

--- admin/src/test/java/top/xlaoer/shortlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/top/xlaoer/shortlink/admin/test/UserTableShardingTest.java
@@ -10,6 +10,7 @@ public class UserTableShardingTest {
             "  origin_url      varchar(1024)                  null comment '原始链接',\n" +
             "  click_num       int default 0                  null comment '点击量',\n" +
             "  gid             varchar(32)                    null comment '分组标识',\n" +
+            "  favicon         varchar(256)                   null comment '网站图标',\n" +
             "  enable_status   tinyint(1)                     null comment '启用标识 0：未启用 1：已启用',\n" +
             "  created_type    tinyint(1)                     null comment '创建类型 0：控制台 1：接口',\n" +
             "  valid_date_type tinyint(1)                     null comment '有效期类型 0：永久有效 1：用户自定义',\n" +
