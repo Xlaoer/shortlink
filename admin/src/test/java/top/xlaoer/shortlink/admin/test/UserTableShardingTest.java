0ef3bc8f7f2584f8173c0326ae94b5aab8cf5793
Xlaoer <makjys@gmail.com>
2024-02-20 16:03:15 +0800

    feature: 基于group id对短链接表进行分表

--- admin/src/test/java/top/xlaoer/shortlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/top/xlaoer/shortlink/admin/test/UserTableShardingTest.java
@@ -2,20 +2,24 @@ package top.xlaoer.shortlink.admin.test;
 
 public class UserTableShardingTest {
 
-    public static final String SQL = "CREATE TABLE `t_user_%d` (\n" +
-            "  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "  `username` varchar(256) DEFAULT NULL COMMENT '用户名',\n" +
-            "  `password` varchar(512) DEFAULT NULL COMMENT '密码',\n" +
-            "  `real_name` varchar(256) DEFAULT NULL COMMENT '真实姓名',\n" +
-            "  `phone` varchar(128) DEFAULT NULL COMMENT '手机号',\n" +
-            "  `mail` varchar(512) DEFAULT NULL COMMENT '邮箱',\n" +
-            "  `deletion_time` bigint(20) DEFAULT NULL COMMENT '注销时间戳',\n" +
-            "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
-            "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
-            "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
-            "  PRIMARY KEY (`id`),\n" +
-            "  UNIQUE KEY `idx_unique_username` (`username`) USING BTREE\n" +
-            ") ENGINE=InnoDB AUTO_INCREMENT=1715030926162935810 DEFAULT CHARSET=utf8mb4;";
+    public static final String SQL = "CREATE TABLE `t_link_%d` (\n" +
+            "  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID' primary key,\n" +
+            "  domain          varchar(128)                   null comment '域名',\n" +
+            "  short_uri       varchar(8) collate utf8mb3_bin null comment '短链接',\n" +
+            "  full_short_url  varchar(128)                   null comment '完整短链接',\n" +
+            "  origin_url      varchar(1024)                  null comment '原始链接',\n" +
+            "  click_num       int default 0                  null comment '点击量',\n" +
+            "  gid             varchar(32)                    null comment '分组标识',\n" +
+            "  enable_status   tinyint(1)                     null comment '启用标识 0：未启用 1：已启用',\n" +
+            "  created_type    tinyint(1)                     null comment '创建类型 0：控制台 1：接口',\n" +
+            "  valid_date_type tinyint(1)                     null comment '有效期类型 0：永久有效 1：用户自定义',\n" +
+            "  valid_date      datetime                       null comment '有效期',\n" +
+            "  `describe`      varchar(1024)                  null comment '描述',\n" +
+            "  create_time     datetime                       null comment '创建时间',\n" +
+            "  update_time     datetime                       null comment '修改时间',\n" +
+            "  del_flag        tinyint(1)                     null comment '删除标识 0：未删除 1：已删除',\n" +
+            "  constraint idx_unique_full_short_url unique (full_short_url)\n" +
+            ") charset = utf8mb4;";
 
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
