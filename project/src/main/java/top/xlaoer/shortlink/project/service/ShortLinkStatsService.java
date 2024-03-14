1c75f9ebf115bfc2dbbfa5a54e8d4203ae1f987d
Xlaoer <makjys@gmail.com>
2024-03-14 13:00:03 +0800

    feature: 开发单个短链接访问监控详情功能

---
+++ project/src/main/java/top/xlaoer/shortlink/project/service/ShortLinkStatsService.java
@@ -0,0 +1,18 @@
+package top.xlaoer.shortlink.project.service;
+
+import top.xlaoer.shortlink.project.dto.req.ShortLinkStatsReqDTO;
+import top.xlaoer.shortlink.project.dto.resp.ShortLinkStatsRespDTO;
+
+/**
+ * 短链接监控接口层
+ */
+public interface ShortLinkStatsService {
+
+    /**
+     * 获取单个短链接监控数据
+     *
+     * @param requestParam 获取短链接监控数据入参
+     * @return 短链接监控数据
+     */
+    ShortLinkStatsRespDTO oneShortLinkStats(ShortLinkStatsReqDTO requestParam);
+}
