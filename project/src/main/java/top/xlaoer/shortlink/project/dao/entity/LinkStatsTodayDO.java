82502de4c11c6e55f65f3867e027991c5a1b8cd5
Xlaoer <makjys@gmail.com>
2024-03-17 18:59:25 +0800

    feature: 开发短链接监控之今日统计

--- project/src/main/java/top/xlaoer/shortlink/project/dao/entity/LinkStatsTodayDO.java
+++ project/src/main/java/top/xlaoer/shortlink/project/dao/entity/LinkStatsTodayDO.java
@@ -1,13 +1,19 @@
 package top.xlaoer.shortlink.project.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import top.xlaoer.shortlink.project.common.database.BaseDO;
 
 import java.util.Date;
 
 @TableName("t_link_stats_today")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LinkStatsTodayDO extends BaseDO {
 
     /**
@@ -43,5 +49,5 @@ public class LinkStatsTodayDO extends BaseDO {
     /**
      * 今日ip数
      */
-    private Integer todayIpCount;
+    private Integer todayUip;
 }
