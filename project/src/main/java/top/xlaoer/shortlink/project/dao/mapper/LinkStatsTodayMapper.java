bec5761e8de514e17403df71ffbce873606a4b53
Xlaoer <makjys@gmail.com>
2024-03-17 17:28:50 +0800

    feature: 设计短链接监控今日以及历史访问信息基础字段（短链接表新增历史统计字段total_pv total_uv total_uip、增加每日统计表）

---
+++ project/src/main/java/top/xlaoer/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
@@ -0,0 +1,7 @@
+package top.xlaoer.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import top.xlaoer.shortlink.project.dao.entity.LinkStatsTodayDO;
+
+public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDO> {
+}
