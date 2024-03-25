06738d84203312c7d313769c5e3dc713dca2a878
Xlaoer <makjys@gmail.com>
2024-03-25 18:08:36 +0800

    feature: 短链接中台创建短链接接口请求限制（引入Sentinel进行流控）

--- project/src/main/java/top/xlaoer/shortlink/project/controller/ShortLinkController.java
+++ project/src/main/java/top/xlaoer/shortlink/project/controller/ShortLinkController.java
@@ -1,5 +1,6 @@
 package top.xlaoer.shortlink.project.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
@@ -15,6 +16,7 @@ import top.xlaoer.shortlink.project.dto.resp.ShortLinkBatchCreateRespDTO;
 import top.xlaoer.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
 import top.xlaoer.shortlink.project.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import top.xlaoer.shortlink.project.dto.resp.ShortLinkPageRespDTO;
+import top.xlaoer.shortlink.project.handler.CustomBlockHandler;
 import top.xlaoer.shortlink.project.service.ShortLinkService;
 
 import java.util.List;
@@ -32,6 +34,11 @@ public class ShortLinkController {
      * 创建短链接
      */
     @PostMapping("/api/short-link/v1/create")
+    @SentinelResource(
+            value = "create_short-link",
+            blockHandler = "createShortLinkBlockHandlerMethod",
+            blockHandlerClass = CustomBlockHandler.class
+    )
     public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
         return Results.success(shortLinkService.createShortLink(requestParam));
     }
