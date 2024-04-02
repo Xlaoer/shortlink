1dbcd0e77a4d8253c0e6f08247ff988d947cd242
Xlaoer <makjys@gmail.com>
2024-04-02 13:18:02 +0800

    feature: 引入网关改造短链接服务

--- admin/src/main/java/top/xlaoer/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/top/xlaoer/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,17 +1,13 @@
 package top.xlaoer.shortlink.admin.common.biz.user;
 
 import cn.hutool.core.util.StrUtil;
-import com.alibaba.fastjson2.JSON;
-import com.google.common.collect.Lists;
-import jakarta.servlet.*;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.StringRedisTemplate;
-
-import java.io.IOException;
-import java.util.List;
-
-import static top.xlaoer.shortlink.admin.common.constant.RedisCacheConstant.USER_LOGIN_KEY;
+import lombok.SneakyThrows;
 
 /**
  * 用户信息传输过滤器
@@ -19,34 +15,20 @@ import static top.xlaoer.shortlink.admin.common.constant.RedisCacheConstant.USER
 @RequiredArgsConstructor
 public class UserTransmitFilter implements Filter {
 
-    private final StringRedisTemplate stringRedisTemplate;
-
-    private static final List<String> IGNORE_URI = Lists.newArrayList(
-            "/api/short-link/admin/v1/user/login",
-            "/api/short-link/admin/v1/user/has-username",
-            "/api/short-link/admin/v1/user"
-    );
-
+    @SneakyThrows
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        // TODO：后续要在网关这里去让用户进行登录，而不是直接写死在这里
-        String requestURI = httpServletRequest.getRequestURI();
-        if (!IGNORE_URI.contains(requestURI)) {
-            String username = httpServletRequest.getHeader("username");
-            if(StrUtil.isNotBlank(username)) {
-                String token = httpServletRequest.getHeader("token");
-                Object userInfoJsonStr = stringRedisTemplate.opsForHash().get(USER_LOGIN_KEY + username, token);
-                if (userInfoJsonStr != null) {
-                    UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
-                    UserContext.setUser(userInfoDTO);
-                }
-            }
+        String username = httpServletRequest.getHeader("username");
+        if (StrUtil.isNotBlank(username)) {
+            String userId = httpServletRequest.getHeader("userId");
+            String realName = httpServletRequest.getHeader("realName");
+            UserInfoDTO userInfoDTO = new UserInfoDTO(userId, username, realName);
+            UserContext.setUser(userInfoDTO);
         }
         try {
             filterChain.doFilter(servletRequest, servletResponse);
         } finally {
-            // 防止内存泄露
             UserContext.removeUser();
         }
     }
